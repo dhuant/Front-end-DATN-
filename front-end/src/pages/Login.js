ab7c5612fb3624d5815f535939d22b1314017fe3
dhuant <1512189@student.hcmus.edu.vn>
2019-03-16 16:41:51 +0700

    Hoan thanh dang nhap bang tai khoan email vs password

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,7 +1,75 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            error: ''
+        };
+    }
+    componentDidMount() {
+        if (localStorage.getItem('token') === 'true') {
+            // console.log('logged');
+            this.props.history.push('/');
+        }
+    }
+    handleEmailChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handlePasswordChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            password: e.target.value
+        })
+    }
+    signIn = (e) => {
+        e.preventDefault();
+        var headers = {
+
+            "Access-Control-Allow-Origin": "*",
+        }
+        //alert('Email address is ' + this.state.email + ' Password is ' + this.state.password);
+        axios.post('http://localhost:3001/users/login',{
+            email: this.state.email,
+            password: this.state.password
+        }, headers)
+            .then(res => {
+                console.log(res);
+                if (res.data.status === 200) {
+                    localStorage.setItem('token', true);
+                    // console.log(res.data.result);
+                    this.props.history.push('/');
+                } else {
+                    this.setState({
+                        error: 'Auth failed!!'
+                    });
+                }
+            })
+            .catch((err) => {
+                console.log("AXIOS ERROR: ", err);
+            });
+        // var data = {
+        //     email:  'trantandat@gmail.com',
+        //     password: '1234',
+        //   }
+        //   var header = {
+        //     'Access-Control-Allow-Origin': '*',
+        //   }
+        //   axios.post('http://localhost:3001/users/login', data, header)
+        //   .then(response=>{
+        //     console.log(response);
+        //   })
+        //   .catch(err=>{
+        //     console.log(err);
+        //   })
+    }
     render() {
+
         return (
             <div className="content-area">
                 <div className="container">
@@ -22,7 +90,7 @@ class Login extends Component {
                                         action="http://themevessel-item.s3-website-us-east-1.amazonaws.com/nest/index.html"
                                         method="GET"
                                     >
-                                        <div style={{marginBottom:'10px'}}>
+                                        <div style={{ marginBottom: '10px' }}>
                                             <a className="btn-google m-b-20" href>
                                                 <img src="images/icons/icon-google.png" alt="GOOGLE" />
                                                 Google
@@ -30,6 +98,7 @@ class Login extends Component {
                                         </div>
                                         <div className="form-group">
                                             <input
+                                                onChange={this.handleEmailChange}
                                                 type="email"
                                                 name="email"
                                                 className="input-text"
@@ -38,6 +107,7 @@ class Login extends Component {
                                         </div>
                                         <div className="form-group">
                                             <input
+                                                onChange={this.handlePasswordChange}
                                                 type="password"
                                                 name="Password"
                                                 className="input-text"
@@ -61,6 +131,7 @@ class Login extends Component {
                                         </div>
                                         <div className="form-group">
                                             <button
+                                                onClick={this.signIn}
                                                 type="submit"
                                                 className="button-md button-theme btn-block"
                                             >
--- front-end/src/pages/Map.js
+++ front-end/src/pages/Map.js
@@ -8,7 +8,7 @@ class Map extends Component {
                 isMarkerShown
 				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCaznvdfOL3vMLdqR729vJEWauyZp9-Ud8&v=3.exp&libraries=geometry,drawing,places`}
 				loadingElement={<div style={{ height: `100%` }} />}
-				containerElement={<div style={{ height: `600px`, width: `100%` }} />}
+				containerElement={<div style={{ height: `600px`, width: `70%` }} />}
 				mapElement={<div style={{ height: `100%` }} />}
 			/>
 		);
