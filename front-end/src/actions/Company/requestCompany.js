825fd2ac960f4c0b1eaaa9d0413ebe95c3da1bb5
dhuant <1512189@student.hcmus.edu.vn>
2019-05-24 14:41:20 +0700

    Sửa request api, lưu thông tin tài khoản công ty xuống redux

--- front-end/src/actions/Company/requestCompany.js
+++ front-end/src/actions/Company/requestCompany.js
@@ -3,20 +3,17 @@ import axios from 'axios'
 import { authCompany } from "../../constants/Company/authCompany";
 import * as config from '../../constants/Config'
 
-export const actGetInfoUserCompany = (id)=>{
+export const actGetInfoUserCompany = ()=>{
     return dispatch => {
       // return console.log("Company")
         return axios
           .get(`${config.API_URL}/company/info`, { headers: authCompany() })
           .then(res => {
+            console.log(res);
             dispatch(action.actSaveInfoUserCompany(res.data));
-            console.log(res.data);
+            
           })
           .catch(err => {
-            // if (err.data.status === 401) {
-            //   console.log(err)
-            //   // dispatch(action.actSaveInfoUserCompany(err.data))
-            // }
             console.log(err.respone)
           })
       };
--- front-end/src/reducers/Company/userCompany.js
+++ front-end/src/reducers/Company/userCompany.js
@@ -4,7 +4,7 @@ var initialState = {};
 const userCompany = (state = initialState, action) => {
     switch (action.type) {
         case Types.SAVE_INFO_USER_COMPANY:
-            state = action.user;
+            state = action.userCompany;
             console.log(state);
             return {...state};
         default: return {...state};
