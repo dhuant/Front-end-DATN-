c433e0f7286b550079606a327c46570c5bede457
dhuant <1512189@student.hcmus.edu.vn>
2019-06-14 16:12:26 +0700

    Sửa giao diện component agent

--- front-end/src/components/Contact/Agent.js
+++ front-end/src/components/Contact/Agent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-
+import moment from 'moment'
 class Agent extends Component {
     render() {
         let { agent } = this.props;
@@ -11,14 +11,14 @@ class Agent extends Component {
         if (agent.company !== "0") {
             company =
                 <li>
-                    <strong>Công ty:</strong>{agent.company}
+                    {/* <strong>Công ty:</strong> */}
                 </li>
         }
         let address =
             <li>
                 <strong>Địa chỉ:</strong>Đang cập nhật
             </li>;
-        if (agent.address !== '') {
+        if (agent.address !== "" && agent.address !== " ") {
             address =
                 <li>
                     <strong>Địa chỉ:</strong>{agent.address}
@@ -28,7 +28,7 @@ class Agent extends Component {
             <li>
                 <strong>Điện thoại:</strong>Đang cập nhật
             </li>;
-        if (agent.phone !== '') {
+        if (agent.phone !== ' ' && agent.phone !== "") {
             mobile =
                 <li>
                     <strong>Điện thoại:</strong>{agent.phone}
@@ -40,23 +40,29 @@ class Agent extends Component {
                 <div>
                 <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                         {/* Agent box 2start */}
-                        <div className="agent-2 clearfix" style={{ height: '231.25px' }}>
+                        <div className="agent-2 clearfix" style={{ height: '231.3px' }}>
                             <div className="col-lg-5 col-md-5 col-sm-4 agent-theme-2">
                                 <img src={agent.avatar} alt="team-2" className="img-responsive" />
                                 {/* social list */}
 
                             </div>
-                            <div className="col-lg-7 col-md-7 col-sm-8 agent-content" style={{ padding: '15px 15px 15px 15px' }}>
+                            <div className="col-lg-7 col-md-7 col-sm-8 agent-content" style={{ padding: '7px 15px 7px 15px' }}>
                                 <h3>
                                     {agent.fullname}
                                 </h3>
-                                <ul>
+                                <ul style={{marginTop:'-7px'}}>
                                     {address}
                                     <li>
                                         <strong>Email:</strong> {agent.email}
                                     </li>
                                     {mobile}
+                                    <li>
+                                        <strong>Số bài đăng:</strong> {agent.totalProject}
+                                    </li>
                                     {company}
+                                    {/* <li>
+                                        <strong>Ngày tham gia:</strong> {moment.unix(agent.createTime).format('DD/MM/YYYY, h:mm a')}
+                                    </li> */}
                                 </ul>
                             </div>
                         </div>
