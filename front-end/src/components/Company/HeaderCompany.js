3c32f5d44181ab4c06c48ba4ee6dd0e940c09549
dhuant <1512189@student.hcmus.edu.vn>
2019-06-20 13:40:45 +0700

    sửa header company

--- front-end/src/components/Company/HeaderCompany.js
+++ front-end/src/components/Company/HeaderCompany.js
@@ -54,11 +54,7 @@ class HeaderCompany extends Component {
     onAfterLogin = (token) => {
         if (token !== null)
             return (<React.Fragment>
-                <li>
-                    <a href="true" onClick={this.onHandleProfile} className="button" style={{ marginRight: '5px' }}>
-                        <i className="fa fa-user" /> Tài khoản
-                    </a>
-                </li>
+                
                 <li>
                     <a href="true" onClick={this.onSignOut} className="button-signout" style={{ marginRight: '5px', color: 'red', border: '1px solid red' }}>
                         <i className="fa fa-sign-out" /> Đăng xuất
